93f80c53b48dac700e444dfc6acfcb6d4ccbbfc3
Camilo Mayorquín <camayorquina@unal.edu.co>
2022-12-05 23:08:18 +0000

    adding first course files

---
+++ Platzi/Curso de Fundamentos de Python/01_print.py
@@ -0,0 +1,24 @@
+print("Hola, esto es el archivo 01")
+
+print("Hola soy Nicolas y tengo 12 años")
+
+# peraciones
+
+#print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+#  Esto es un comentario
+
+"""
+varias
+lineas
+"""
+
+'''
+otra manera
+de comentar
+varias lineas
+'''
+
---
+++ Platzi/Curso de Fundamentos de Python/main.py
@@ -0,0 +1,24 @@
+print("Hola, esto es python")
+
+print("Hola soy Nicolas y tengo 12 años")
+
+# peraciones
+
+#print(12 + 5)
+print(10 - 5)
+print(2 * 3)
+print(8 / 2)
+
+#  Esto es un comentario
+
+"""
+varias
+lineas
+"""
+
+'''
+otra manera
+de comentar
+varias lineas
+'''
+
