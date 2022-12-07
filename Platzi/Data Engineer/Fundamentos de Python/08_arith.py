7c6e9ad1233627fc4a1ec09769aa54d3c6796b52
Camilo Mayorquín <camayorquina@unal.edu.co>
2022-12-07 01:41:33 +0000

    arithmetic

---
+++ Platzi/Data Engineer/Fundamentos de Python/08_arith.py
@@ -0,0 +1,15 @@
+print(10 + 10)
+print(10 - 5)
+print(10 * 2)
+print(10 / 2)
+print(10 % 2)
+print(10 / 3)
+print(10 % 3)
+print(10 // 3)
+print(2 ** 3 + 3 - 7 / 1 // 4) # PEMDAS hierarchy
+
+print('Hola' + ' mundo')
+print('Hola' * 3)
+print(2 ** 3)
+
+print()
