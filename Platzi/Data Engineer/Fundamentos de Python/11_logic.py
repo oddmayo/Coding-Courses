d3ea1c222ed91a7fdd22a6ccc7e231acab3af85c
Camilo Mayorquín <camayorquina@unal.edu.co>
2022-12-07 03:02:24 +0000

    logic

---
+++ Platzi/Data Engineer/Fundamentos de Python/11_logic.py
@@ -0,0 +1,19 @@
+print(True and True)
+print(True and False)
+print(False and False)
+
+print(10 > 5 and 5 < 10)
+
+stock = input("Ingrese el número de stock => ")
+stock = int(stock)
+
+print(stock >= 100 and stock <=1000)
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+
+role = input("Digital el rol: ")
+
+print(role == "admin" or role == "seller")
