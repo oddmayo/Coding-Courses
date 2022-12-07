668ff15c30fad999e4d92f20980c1a4ad644557e
Camilo Mayorquín <camayorquina@unal.edu.co>
2022-12-07 01:34:11 +0000

    transformations

---
+++ Platzi/Data Engineer/Fundamentos de Python/07_transformation.py
@@ -0,0 +1,21 @@
+name = "Camilo"
+print(type(name))
+name = 12
+print(type(name))
+name = True
+print(type(name))
+
+print("Camilo" + " Mayorquín")
+print(10 + 20)
+print("Camilo"+"12") # same data type
+
+age = 12
+print("Mi edad es " + str(age))
+print(f"Mi edad es {age}")
+
+age = input("Escribe tu edad => ")
+print(type(age))
+age = int(age)
+age += 10
+print(f"Tu edad en 10 años será {age}")
+
