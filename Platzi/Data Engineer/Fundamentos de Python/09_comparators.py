661198e56232f8b0953c7dbf811e08c9911324a5
Camilo Mayorquín <camayorquina@unal.edu.co>
2022-12-07 02:33:51 +0000

    comparators

---
+++ Platzi/Data Engineer/Fundamentos de Python/09_comparators.py
@@ -0,0 +1,17 @@
+print(7 > 3)
+print(3 > 7)
+print(7 > 7)
+
+print(2 <= 3)
+print(2 <= 2)
+
+print( 6 == 6)
+print( 6 == 2)
+
+print( 6 != 10)
+print( 6 != 6)
+
+print("APPLE" == "Apple")
+print("Apple" == "Apple")
+
+print(1 == '1')
