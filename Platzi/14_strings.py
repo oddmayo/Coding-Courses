8d85787ab430d6cc22ffc8018d710b6e4cec62c5
Camilo Mayorquín <camayorquina@unal.edu.co>
2023-01-17 17:32:34 +0000

    strings

---
+++ Platzi/14_strings.py
@@ -0,0 +1,30 @@
+text = 'Ella sabe programar en Python'
+
+'''
+print('JavaScript' in text)
+print('Python' in text)
+
+if 'Python' in text:
+    print('Elegiste bien!')
+else:
+    print('Elegiste mal')
+
+size = len('amor')
+print(size)
+'''
+
+print(text.upper())
+print(text.lower())
+
+print(text.count('a'))
+print(text.swapcase())
+print(text.startswith('Ella'))
+print(text.endswith('Rust'))
+print(text.replace('Python','Go'))
+
+text_2 = 'este es un titulo'
+print(text_2.capitalize())
+print(text_2.title())
+
+print(text_2.isdigit())
+print("398".isdigit())
--- Udacity/Challenge/Introduction to SQL/intro.sql
+++ Udacity/Challenge/Introduction to SQL/intro.sql
@@ -7,4 +7,17 @@ FROM orders
 WHERE (standard_qty = 0 OR gloss_qty = 0 OR poster_qty = 0)
 AND occurred_at = '2016-10-01'
 
+--
+
+-- my first query
+
+SELECT id, account_id, occurred_at
+FROM orders;
+
+-- use of LIMIT: always at the end of the query
+
+SELECT *
+FROM web_events
+LIMIT 100000;
+
 
