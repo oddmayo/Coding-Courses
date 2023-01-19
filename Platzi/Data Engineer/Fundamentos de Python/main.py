2646159af6643b5e7949fc1235b58d97f230777c
Camilo Mayorquín <camayorquina@unal.edu.co>
2023-01-19 02:49:54 +0000

    adding random option for computer

--- Platzi/Data Engineer/Fundamentos de Python/main.py
+++ Platzi/Data Engineer/Fundamentos de Python/main.py
@@ -1,5 +1,17 @@
+import random
+
+options = ('piedra','papel','tijera')
+
 user_option = input('piedra, papel o tijera ')
-computer_option = 'piedra'
+user_option = user_option.lower()
+
+if not user_option in options:
+    print('opción no válida')
+
+computer_option = random.choice(options)
+
+print('User option: ', user_option)
+print('Computer option: ',computer_option)
 
 if user_option == computer_option:
     print('Empate')
